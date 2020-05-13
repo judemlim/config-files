b5e0bee24299ea043751a18e7d78d810484000d2
Jude Lim <judemlim@gmail.com>
2020-05-13 11:56:03 +1000

    updates

---
+++ picom/launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Terminate already running picom
+killall -q picom
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x picom >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+picom &
+
+echo "Polybar launched..."
